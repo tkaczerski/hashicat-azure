75672e1e19cc6abb0aec1fffd66b90d16cae3f2e
root <root@p-xhxmqpkrgusq-workstation.c.instruqt-prod.internal>
2021-08-20 16:59:20 +0000

    Added devops Department and billable tag.

--- main.tf
+++ main.tf
@@ -110,10 +110,9 @@ resource "azurerm_virtual_machine" "catapp" {
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
-  tags {
-    Department = "devops"
-    Billable   = "true"
-  }
+  Department                    = "devops"
+  Billable                      = "true"
+
   storage_image_reference {
     publisher = var.image_publisher
     offer     = var.image_offer
