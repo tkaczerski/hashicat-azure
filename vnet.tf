0789d408b1bf0a2bdf2729514ac57b133854bc8f
root <root@p-xhxmqpkrgusq-workstation.c.instruqt-prod.internal>
2021-08-20 17:33:19 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source              = "app.terraform.io/bryce-training/network/azurerm"
+  source              = "app.terraform.io/tedkaczerski-training/network/azurerm"
   version             = "3.0.1"
   resource_group_name = "${var.prefix}-workshop"
 }
